861f2775caad0ca6372fce595a1e3e355ef1f8a4
Kylie Stanley <knstanley11@gmail.com>
2019-01-06 22:01:44 -0700

    Updates

---
+++ precache-manifest.00f5f98d89d8c7126e516ae166126e00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef8128212ef0c4545084",
+    "url": "/quiz-me/static/js/runtime~main.ef812821.js"
+  },
+  {
+    "revision": "672f8a7714a666940488",
+    "url": "/quiz-me/static/js/main.672f8a77.chunk.js"
+  },
+  {
+    "revision": "3a58f11f13f5574bd1fa",
+    "url": "/quiz-me/static/js/1.3a58f11f.chunk.js"
+  },
+  {
+    "revision": "672f8a7714a666940488",
+    "url": "/quiz-me/static/css/main.7530a1e9.chunk.css"
+  },
+  {
+    "revision": "c97ba41675aeb92acbfde534384058c5",
+    "url": "/quiz-me/index.html"
+  }
+];
