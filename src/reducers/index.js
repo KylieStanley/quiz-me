082d4ab6c457b2fd3b92c7babf4272dca79eda2a
Kylie Stanley <knstanley11@gmail.com>
2019-04-02 11:52:51 -0600

    Add allQuestions to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,13 @@
 import { combineReducers } from 'redux';
-import { isLoading, hasErrored, questions } from './questionReducer';
+import {
+  isLoading,
+  hasErrored,
+  questions,
+  allQuestions
+} from './questionReducer';
 
 const rootReducer = combineReducers({
+  allQuestions,
   questions,
   isLoading,
   error: hasErrored
