ece0179fbbb289dbaf56bcc3f5d1c424c7744f29
Kylie Stanley <knstanley11@gmail.com>
2019-04-03 22:47:02 -0600

    Add mapStateToProps to make use of Redux store

--- src/AnswerBank.js
+++ src/AnswerBank.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Answer from './Answer.js';
 import PropTypes from 'prop-types';
 import './styles/main.scss';
 
-
-export default class AnswerBank extends Component {
+class AnswerBank extends Component {
   constructor(props) {
     super(props);
     this.state = {
       answers: null
-    }
+    };
   }
 
-  shuffleArray = (array) => {
+  shuffleArray = array => {
     let i = array.length - 1;
     for (; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -21,17 +21,17 @@ export default class AnswerBank extends Component {
       array[j] = temp;
     }
     return array;
-  }
+  };
 
-  componentWillMount () {
-    this.setState({ answers: this.shuffleArray(this.props.answers)});
+  componentWillMount() {
+    this.setState({ answers: this.shuffleArray(this.props.answers) });
   }
 
   componentDidUpdate(prevProps) {
     if (this.props.answers !== prevProps.answers) {
       this.setState({
         answers: this.props.answers
-      })
+      });
     }
   }
 
@@ -39,32 +39,33 @@ export default class AnswerBank extends Component {
     return (
       <div className="answer-wrapper">
         <div className="answer-container">
-        {
-          this.state.answers.map(answer => {
-            return <Answer answer={answer} 
-                           currentQuestion={this.props.currentQuestion}
-                           validateAnswer={this.props.validateAnswer} 
-                           minutes={this.props.minutes}
-                           seconds={this.props.seconds} />
-          })
-        }
+          {this.state.answers.map(answer => {
+            return (
+              <Answer
+                answer={answer}
+                currentQuestion={this.props.currentQuestion}
+                validateAnswer={this.props.validateAnswer}
+                minutes={this.props.minutes}
+                seconds={this.props.seconds}
+              />
+            );
+          })}
         </div>
       </div>
     );
   }
 }
 
+export const mapStateToProps = state => ({
+  answers: state.answers
+});
+
+export default connect(mapStateToProps)(AnswerBank);
 
 AnswerBank.propTypes = {
-  minutes: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number,
-  ]),
-  seconds: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number,
-  ]),
+  minutes: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  seconds: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   validateAnswer: PropTypes.func,
   currentQuestion: PropTypes.object,
   answers: PropTypes.array
-}
+};
